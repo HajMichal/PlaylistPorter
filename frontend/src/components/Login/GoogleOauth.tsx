a15aa9184d1e68ca6de0a380165d182520ab320a
HajMichal <michalhaj2003@gmail.com>
2024-05-29 03:21:33 +0200

    [INITIAL]: done: authentication with google, created prototype of frotnend

--- frontend/src/components/Login/GoogleOauth.tsx
+++ frontend/src/components/Login/GoogleOauth.tsx
@@ -1,10 +1,13 @@
-export const GoogleOauth = () => {
+function GoogleOauth() {
   const login = () => {
     window.location.href = "http://localhost:3000/auth/google";
   };
+
   return (
     <div>
       <button onClick={() => login()}>test</button>
     </div>
   );
-};
+}
+
+export default GoogleOauth;
